d14e0c43258e979edf9209be55c9f20437155470
Richard van der Dys <richard.vanderdys@gmail.com>
2017-09-13 00:26:17 +0300

    Saving and loading data

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    const saved = JSON.parse(localStorage.getItem('state') || {});
     this.state = {
-      data: {},
+      data: saved.data || {},
       editing: undefined,
-      width: 5,
-      height: 10,
+      width: saved.width || 5,
+      height: saved.height || 10,
     };
+  }
+  savedState() {
+    const jsonState = JSON.stringify({
+      data: this.state.data,
+      width: this.state.width,
+      height: this.state.height,
+    });
+    localStorage.setItem('state', jsonState);    
   }
   handleChange(id, value) {
     this.setState({
       data: Object.assign({}, this.state.data, { [id]: value }),
-    });
+    }, this.savedState.bind(this));
   }
   render() {
     return <Table
@@ -25,9 +34,9 @@ class App extends Component {
       handleChange={this.handleChange.bind(this)}
       handleFocus={id => this.setState({ editing: id })}
       handleBlur={() => this.setState({ editing: undefined })}
-      handleClear={() => this.setState({ data: {}, editing: undefined })}
-      addColumn={() => this.setState({ width: this.state.width + 1 })}
-      addRow={() => this.setState({ height: this.state.height + 1 })}
+      handleClear={() => this.setState({ data: {}, editing: undefined }, this.savedState.bind(this))}
+      addColumn={() => this.setState({ width: this.state.width + 1 }, this.savedState.bind(this))}
+      addRow={() => this.setState({ height: this.state.height + 1 }, this.savedState.bind(this))}
       data={this.state.data}
     />;
   }
