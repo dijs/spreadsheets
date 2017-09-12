9b9ed0f350fae7718fd62110b57aee4396940944
Richard van der Dys <richard.vanderdys@gmail.com>
2017-09-12 20:52:17 +0300

    Refactored and added state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Table from './Table';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleChange(id, value) {
+    this.setState({ [id]: value });
+  }
   render() {
-    const rows = [];
-    rows.push(
-      <tr>
-        <th/>
-        {
-          Array(4).fill(0).map((v, x) => <th>{String.fromCharCode('A'.charCodeAt(0) + x)}</th>)
-        }
-      </tr>
-    );
-    for (let y = 0; y < 4; y++) {
-      rows.push(
-        <tr>
-          <td className="row-label">{y + 1}</td>
-          {
-            Array(4).fill(0).map((v, x) => <td><input type="text" className="cell" /></td>)
-          }
-        </tr>
-      );
-    }
-    return (
-      <div className="App">
-        <table>
-          {rows}
-        </table>
-      </div>
-    );
+    return <Table width={5} height={10} handleChange={this.handleChange.bind(this)} data={this.state} />;
   }
 }
 
---
+++ src/Table.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function toRow(n) {
+  return String.fromCharCode('A'.charCodeAt(0) + n);
+}
+
+export default function Table({ width, height, handleChange, data }) {
+  const rows = [];
+  rows.push(
+    <tr key="row0">
+      <th/>
+      {
+        Array(width).fill(0).map((v, x) => <th key={'col' + x}>{toRow(x)}</th>)
+      }
+    </tr>
+  );
+  for (let y = 1; y <= height; y++) {
+    rows.push(
+      <tr key={'row' + y}>
+        <td className="row-label">{y}</td>
+        {
+          Array(width).fill(0).map((v, x) => {
+            const id = toRow(x) + y;
+            const value = data[id] || '';
+            return <td key={id}>
+              <input type="text" className="cell" onChange={e => handleChange(id, e.target.value)} value={value} />
+            </td>;
+          })
+        }
+      </tr>
+    );
+  }
+  return (
+    <table>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  );
+}
